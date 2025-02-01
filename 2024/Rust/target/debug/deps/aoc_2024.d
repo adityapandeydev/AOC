e262730185c3ba0d2735338f35bff17c432bc491
Aditya Pandey <adityapandey.inbox@gmail.com>
2025-02-02 02:25:01 +0530

    Remove target folder from Git tracking and rust day 1 part 1

--- 2024/Rust/target/debug/deps/aoc_2024.d
+++
@@ -1,7 +0,0 @@
-D:\Aditya\Projects\AOC\2024\Rust\target\debug\deps\aoc_2024.exe: main.rs day1\mod.rs day1\part1.rs
-
-D:\Aditya\Projects\AOC\2024\Rust\target\debug\deps\aoc_2024.d: main.rs day1\mod.rs day1\part1.rs
-
-main.rs:
-day1\mod.rs:
-day1\part1.rs:
--- 2024/Rust/target/debug/deps/rust.d
+++
@@ -1,8 +0,0 @@
-D:\Aditya\Projects\AOC\2024\Rust\target\debug\deps\rust.exe: main.rs day1\mod.rs day1\part1.rs utils.rs
-
-D:\Aditya\Projects\AOC\2024\Rust\target\debug\deps\rust.d: main.rs day1\mod.rs day1\part1.rs utils.rs
-
-main.rs:
-day1\mod.rs:
-day1\part1.rs:
-utils.rs:
