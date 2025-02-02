ca1e2603466c860b986c228123ecf6a7d3b1e37e
Aditya Pandey <adityapandey.inbox@gmail.com>
2025-02-03 05:29:55 +0530

    add python util module to avoid code repetition

--- 2024/Python/day2part1.py
+++ 2024/Python/day2part1.py
@@ -1,14 +1,4 @@
-def isSafe(report):
-    left = report[0]
-    right = report[1]
-    isIncreasing = right > left
-    for i in range(len(report) - 1):
-        left = report[i]
-        right = report[i + 1]
-        difference = abs(right - left) >= 1 and abs(right - left) <= 3
-        if isIncreasing and left > right or not isIncreasing and right > left or not difference:
-            return False
-    return True
+import utils
 
 count = 0;
 report = []
@@ -16,6 +6,6 @@ file = open("2024/Input/day2.1.txt", "r")
 for line in file:
     line = line.strip()
     report = list(map(int, line.split(" ")))
-    if isSafe(report):
+    if utils.isSafe(report):
         count = count + 1
 print(count)        
