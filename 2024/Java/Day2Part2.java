b2d9c93c4571e46c932dfe67f6bcc67ad73aa67f
Aditya Pandey <adityapandey.inbox@gmail.com>
2025-02-03 01:19:46 +0530

    day 2 java code better implementation

--- 2024/Java/Day2Part2.java
+++ 2024/Java/Day2Part2.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.nio.file.*;
 import java.util.*;
 
-public class Day2Part1 {
+public class Day2Part2 {
     public static void main(String[] args) throws IOException{
         ArrayList<String[]> reports = new ArrayList<>();
         try (BufferedReader reader = Files.newBufferedReader(Path.of("2024/Input/day2.1.txt"))) {
@@ -14,6 +14,7 @@ public class Day2Part1 {
 
         int count = 0;
         for (String[] report : reports) {
+            int damper = 1;
             boolean flag = true;
             int left = Integer.parseInt(report[0]);
             int right = Integer.parseInt(report[1]);
@@ -21,24 +22,40 @@ public class Day2Part1 {
             boolean difference = (Math.abs(right - left) >= 1) && (Math.abs(right - left) <= 3);
             for (int i = 1; i < report.length - 1; i++) {
                 if (!difference) {
-                    flag = false;
-                    break;
+                    if (damper == 0) {
+                        flag = false;
+                        break;
+                    }
+                    damper--;
                 }
                 left = Integer.parseInt(report[i]);
                 right = Integer.parseInt(report[i + 1]);
                 difference = (Math.abs(right - left) >= 1) && (Math.abs(right - left) <= 3);
                 if (isIncreasing && (left > right)) {
-                    flag = false;
-                    break;
+                    if (damper == 0) {
+                        flag = false;
+                        break;
+                    }
+                    damper--;
                 } else if (!isIncreasing && (right > left)) {
-                    flag = false;
-                    break;
+                    if (damper == 0) {
+                        flag = false;
+                        break;
+                    }
+                    damper--;
                 } else if (!difference) {
-                    flag = false;
-                    break;
+                    if (damper == 0) {
+                        flag = false;
+                        break;
+                    }
+                    damper--;
                 }
             }
             if (flag) {
+                for (String ch : report) {
+                    System.out.print(ch + " ");
+                }
+                System.out.println();
                 count++;
             }
         }
